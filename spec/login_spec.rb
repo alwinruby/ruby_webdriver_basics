067db0c6638d6c8cacf67f50e6f30f7452d4d0c0
alwinruby <alwinsolanky@gmail.com>
2017-02-22 12:12:11 +0000

    refactored out

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -1,11 +1,13 @@
 # filename: login_spec.rb
 
 require 'selenium-webdriver'
+require 'login'
 
 describe 'Login' do
   before(:each) do
     geckodriver = File.join(Dir.pwd, 'vendor', 'geckodriver')
     @driver = Selenium::WebDriver.for :firefox, driver_path: geckodriver
+    @login = Login.new(@driver)
   end
 
   after(:each) do
@@ -13,11 +15,8 @@ describe 'Login' do
   end
 
   it 'succeeded' do
-    @driver.get 'http://the-internet.herokuapp.com/login'
-    @driver.find_element(id: 'username').send_keys('tomsmith')
-    @driver.find_element(id: 'password').send_keys('SuperSecretPassword!')
-    @driver.find_element(css: 'button').submit
+    @login.with('tomsmith', 'SuperSecretPassword!')
     sleep(1)
-    expect(@driver.find_element(css: '.flash.success').displayed?).to eql true
+    expect(@login.success_message_present?).to eql true
   end
 end
