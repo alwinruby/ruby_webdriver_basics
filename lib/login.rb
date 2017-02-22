4dc304275232cb263e8fe07ecb755ac63b474257
alwinruby <alwinsolanky@gmail.com>
2017-02-22 12:28:54 +0000

    Failue test

--- lib/login.rb
+++ lib/login.rb
@@ -1,6 +1,8 @@
 # filename: login.rb
 
 class Login
+
+  LOGIN_FORM      = { id: 'login' }
   USERNAME_INPUT  = { id: 'username' }
   PASSWORD_INPUT  = { id: 'password' }
   SUBMIT_BUTTON   = { css: 'button'  }
@@ -10,6 +12,8 @@ class Login
     def initialize(driver)
       @driver = driver
       @driver.get 'http://the-internet.herokuapp.com/login'
+      raise 'Login page not ready' unless
+        @driver.find_element(LOGIN_FORM).displayed?
     end
 
     def with(username, password)
