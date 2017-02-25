fa1c33366a84cf00ed22236059dc2b4da58a52f0
alwinruby <alwinsolanky@gmail.com>
2017-02-25 19:56:43 +0000

    Problems on chapter 13 part 2

---
+++ config_cloud.rb
@@ -0,0 +1,13 @@
+# filename: config_cloud.rb
+
+require 'sauce_whisk'
+
+ENV['base_url']
+ENV['host']
+
+ENV['operating_system'] ||= 'Windows XP'
+ENV['browser'] ||= 'internet_explorer'
+ENV['browser_version'] ||= '8'
+
+ENV['SAUCE_USERNAME'] = 'alwinsol'
+ENV['SAUCE_ACCESS_KEY'] = 'Empire1980'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -101,19 +101,52 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
-  config.before(:each) do
-    # geckodriver = File.join(Dir.pwd, 'vendor', 'geckodriver')
-    # @driver = Selenium::WebDriver.for :firefox, driver_path: geckodriver
-    case ENV['browser']
-    when 'firefox'
-      geckodriver = File.join(Dir.pwd, 'vendor', 'geckodriver')
-      @driver = Selenium::WebDriver.for :firefox, driver_path: geckodriver
-    when 'chrome'
-      chromedriver = File.join(Dir.pwd, 'vendor', 'chromedriver')
-      @driver = Selenium::WebDriver.for :chrome, driver_path: chromedriver end
+  # config.before(:each) do
+  #   # geckodriver = File.join(Dir.pwd, 'vendor', 'geckodriver')
+  #   # @driver = Selenium::WebDriver.for :firefox, driver_path: geckodriver
+  #   case ENV['browser']
+  #   when 'firefox'
+  #     geckodriver = File.join(Dir.pwd, 'vendor', 'geckodriver')
+  #     @driver = Selenium::WebDriver.for :firefox, driver_path: geckodriver
+  #   when 'chrome'
+  #     chromedriver = File.join(Dir.pwd, 'vendor', 'chromedriver')
+  #     @driver = Selenium::WebDriver.for :chrome, driver_path: chromedriver end
+  # end
+
+    config.before(:each) do |example|
+      case ENV['host']
+      when 'saucelabs'
+        caps = Selenium::WebDriver::Remote::Capabilities.send(ENV['browser'])
+        caps.version = ENV['browser_version']
+        caps.platform = ENV['operating_system']
+        caps[:name] = example.metadata[:full_description]
+
+          @driver = Selenium::WebDriver.for(
+            :remote,
+            url: "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}
+      @ondemand.saucelabs.com:80/wd/hub",
+            desired_capabilities: caps)
+      else
+        case ENV['browser']
+        when 'firefox'
+          geckodriver = File.join(Dir.pwd, 'vendor', 'geckodriver')
+          @driver = Selenium::WebDriver.for :firefox, driver_path: geckodriver
+        when 'chrome'
+          chromedriver = File.join(Dir.pwd, 'vendor', 'chromedriver')
+          @driver = Selenium::WebDriver.for :chrome, driver_path: chromedriver
+        end
+      end
   end
 
-  config.after(:each) do
+  config.after(:each) do |example|
+    if ENV['host'] == 'saucelabs'
+      if example.exception.nil?
+        SauceWhisk::Jobs.pass_job @driver.session_id
+      else
+        SauceWhisk::Jobs.fail_job @driver.session_id
+      end
+    end
+
     @driver.quit
   end
 
